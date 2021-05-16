0b82d586f3957e66b2ab85a9add07fe3e44fc159
madzka <maosak@gmail.com>
2021-05-17 01:06:05 +0200

    xx

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,14 +1,19 @@
-define(function() {var keywords=[{w:"oXygen",p:["p0","p6"]},{w:"and",p:["p0","p8"]},{w:"DITA",p:["p0"]},{w:"in",p:["p0"]},{w:"the",p:["p0"]},{w:"process",p:["p0"]},{w:"of",p:["p0"]},{w:"documentation",p:["p0"]},{w:"Tools",p:["p1"]},{w:"installation",p:["p1"]},{w:"Creating",p:["p2","p3","p4","p8"]},{w:"Concept",p:["p2"]},{w:"Topic",p:["p2","p3","p4"]},{w:"Reference",p:["p3"]},{w:"Task",p:["p4"]},{w:"My",p:["p5"]},{w:"first",p:["p5"]},{w:"project",p:["p5"]},{w:"setup",p:["p6"]},{w:"DITA_Map",p:["p7"]},{w:"PDF",p:["p8"]},{w:"HTML",p:["p8"]}];
+define(function() {var keywords=[{w:"oXygen",p:["p0","p13"]},{w:"INSTALLATION",p:["p0"]},{w:"CREATING",p:["p1","p2","p3","p4","p7"]},{w:"YOUR",p:["p1","p2","p3","p4"]},{w:"FIRST",p:["p1","p2","p3","p4"]},{w:"Concept",p:["p1"]},{w:"Topic",p:["p1","p2","p3"]},{w:"REFERENCE",p:["p2"]},{w:"TASK",p:["p3"]},{w:"PROJECT",p:["p4","p8"]},{w:"WORKING",p:["p5"]},{w:"WITH",p:["p5"]},{w:"DOCUMENTS",p:["p5"]},{w:"DITA",p:["p6"]},{w:"MAP",p:["p6"]},{w:"PDF",p:["p7"]},{w:"and",p:["p7"]},{w:"HTML",p:["p7"]},{w:"OUTPUT",p:["p7"]},{w:"MY",p:["p8"]},{w:"GRADUATE",p:["p8"]},{w:"DOCUMANTATION",p:["p9"]},{w:"PLAN",p:["p9"]},{w:"PRODUCT",p:["p10"]},{w:"DESCRIPTION",p:["p10"]},{w:"TOOLS",p:["p11"]},{w:"FOR",p:["p11"]},{w:"DOCUMENTATION",p:["p11"]},{w:"PROCESS",p:["p11"]},{w:"My_first_concept",p:["p12"]},{w:"Showing",p:["p13"]},{w:"full",p:["p13"]},{w:"tags",p:["p13"]},{w:"in",p:["p13"]},{w:"the",p:["p13"]},{w:"author",p:["p13"]},{w:"by",p:["p13"]},{w:"default",p:["p13"]}];
 var ph={};
-ph["p0"]=[0, 1, 2, 3, 4, 5, 6, 7];
-ph["p1"]=[8, 9];
-ph["p2"]=[10, 11, 12];
-ph["p3"]=[10, 13, 12];
-ph["p4"]=[10, 14, 12];
-ph["p5"]=[15, 16, 17];
-ph["p6"]=[0, 18];
-ph["p7"]=[19];
-ph["p8"]=[10, 20, 1, 21];
+ph["p0"]=[0, 1];
+ph["p1"]=[2, 3, 4, 5, 6];
+ph["p2"]=[2, 3, 4, 7, 6];
+ph["p3"]=[2, 3, 4, 8, 6];
+ph["p4"]=[2, 3, 4, 9];
+ph["p5"]=[10, 11, 12];
+ph["p6"]=[13, 14];
+ph["p7"]=[2, 15, 16, 17, 18];
+ph["p8"]=[19, 20, 9];
+ph["p9"]=[21, 22];
+ph["p10"]=[23, 24];
+ph["p12"]=[29];
+ph["p11"]=[25, 26, 27, 28];
+ph["p13"]=[30, 31, 32, 33, 34, 0, 35, 36, 37];
      return {
          keywords: keywords,
          ph: ph
--- oxygen-webhelp/app/search/index/link-to-parent.js
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {3:2,6:2,8:7,0:-1,4:2,5:2,2:-1,1:-1,7:-1};
+return {};
 });
