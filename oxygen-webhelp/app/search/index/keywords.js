971d72dc5819638df5d7a0628450fdb4693635d9
db4rr <dbarr@dolby.com>
2021-05-20 12:00:41 +0200

    Adding fresh output files from repo_Daniel
    I removed whitespaces and special characters from folder names in the source repository, and changed the reference paths in the dita map accordingly.

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,19 @@
+define(function() {var keywords=[{w:"oXygen",p:["p0"]},{w:"INSTALLATION",p:["p0"]},{w:"MY",p:["p1"]},{w:"GRADUATE",p:["p1"]},{w:"PROJECT",p:["p1","p8"]},{w:"DOCUMANTATION",p:["p2"]},{w:"PLAN",p:["p2"]},{w:"PRODUCT",p:["p3"]},{w:"DESCRIPTION",p:["p3"]},{w:"TOOLS",p:["p4"]},{w:"FOR",p:["p4"]},{w:"DOCUMENTATION",p:["p4"]},{w:"PROCESS",p:["p4"]},{w:"CREATING",p:["p5","p6","p7","p8","p11"]},{w:"YOUR",p:["p5","p6","p7","p8"]},{w:"FIRST",p:["p5","p6","p7","p8"]},{w:"Concept",p:["p5"]},{w:"Topic",p:["p5","p6","p7"]},{w:"REFERENCE",p:["p6"]},{w:"TASK",p:["p7"]},{w:"WORKING",p:["p9"]},{w:"WITH",p:["p9"]},{w:"DOCUMENTS",p:["p9"]},{w:"DITA",p:["p10"]},{w:"MAP",p:["p10"]},{w:"PDF",p:["p11"]},{w:"and",p:["p11"]},{w:"HTML",p:["p11"]},{w:"OUTPUT",p:["p11"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2, 3, 4];
+ph["p11"]=[13, 25, 26, 27, 28];
+ph["p2"]=[5, 6];
+ph["p3"]=[7, 8];
+ph["p4"]=[9, 10, 11, 12];
+ph["p5"]=[13, 14, 15, 16, 17];
+ph["p6"]=[13, 14, 15, 18, 17];
+ph["p7"]=[13, 14, 15, 19, 17];
+ph["p8"]=[13, 14, 15, 4];
+ph["p9"]=[20, 21, 22];
+ph["p10"]=[23, 24];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {4:1,11:-1,10:-1,5:9,7:9,2:1,6:9,1:-1,0:-1,9:-1,3:1,8:9};
+});
