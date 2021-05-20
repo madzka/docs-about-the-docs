289102a5db690c990ba3a844022e4f2b96382d7f
db4rr <dbarr@dolby.com>
2021-05-20 11:46:27 +0200

    cleaning up the repo

--- oxygen-webhelp/app/search/index/link-to-parent.js
+++
@@ -1,4 +0,0 @@
-/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-define(function () {
-return {23:20,19:-1,18:-1,21:20,13:17,12:-1,15:17,17:-1,20:-1,14:17,22:20,16:17};
-});
